bfb9287fb032f0bcde84faa52b8c305f4e7d8ae4
Murad <145739695+Murgitadd@users.noreply.github.com>
2023-10-05 01:35:10 +0400

    Done Task2
    Doesnt work on compiler :')

--- Task2.cs
+++ Task2.cs
@@ -1,16 +1,46 @@
-Console.WriteLine("Enter a three-digit number:");
+using System;
 
-int num = int.Parse(Console.ReadLine());
-
-int yuzluk = (num % 1000) / 100;
-int onluq = (num % 100) / 10;
-int teklik = (num % 10);
-
-if (yuzluk == onluq && onluq == teklik)
+class Program
 {
-    Console.WriteLine("Butun ededler eynidir.");
-}
-else
-{
-    Console.WriteLine("Eyni olmayan ededler var!");
+    static void Main()
+    {
+        double circleArea = Area(5.0); 
+        Console.WriteLine("Area of circle: " + circleArea);
+
+        double rectangleArea = Area(4.0, 6.0); 
+        Console.WriteLine("Area of rectangle: " + rectangleArea);
+
+        double parallelepipedArea = Area(3.0, 4.0, 5.0); 
+        Console.WriteLine("Area of parallelepiped: " + parallelepipedArea);
+
+        double triangleArea = Area(3.0, 4.0, 5.0);
+        Console.WriteLine("Area of triangle with nested circle: " + triangleArea);
+    }
+
+    static double Area(double radius)
+    {
+        double pi = Math.PI;
+        return pi * Math.Pow(radius, 2);
+    }
+
+    static double Area(double length, double width)
+    {
+        
+        return length * width;
+    }
+
+    static double Area(double length, double width, double height)
+    {
+
+        return 2 * (length * width + length * height + width * height);
+    }
+
+    static double Area(double sideA, double sideB, double sideC)
+    {
+     
+        double semiPerimeter = (sideA + sideB + sideC) / 2;
+        double radius = semiPerimeter / 2;
+        double areaOfTriangle = Math.Sqrt(semiPerimeter * (semiPerimeter - sideA) * (semiPerimeter - sideB) * (semiPerimeter - sideC));
+        return radius * areaOfTriangle;
+    }
 }

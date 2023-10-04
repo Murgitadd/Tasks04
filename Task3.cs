c063307153575840995802fc2dd0c977cf3f32f4
Murad <145739695+Murgitadd@users.noreply.github.com>
2023-10-05 01:36:00 +0400

    Delete Task3.cs

--- Task3.cs
+++
@@ -1,7 +0,0 @@
-Console.WriteLine("Enter a:");
-int a = int.Parse(Console.ReadLine());
-Console.WriteLine("Enter b:");
-int b = int.Parse(Console.ReadLine());
-
-Console.WriteLine($"a={a+1}");
-Console.WriteLine($"b={b-1}");
